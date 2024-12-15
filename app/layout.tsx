d5f96c51ac4c9b6667c852587092af3faaf35bfd
Tanishka <tanishkadeep09@gmail.com>
2024-12-15 18:34:38 +0530

    added Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/react"
 
 const recoleta = localFont({
   src: "../public/fonts/Recoleta-Bold.ttf",
@@ -19,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${recoleta.variable}`}>{children}</body>
+      <body className={`${recoleta.variable}`}>{children} <Analytics /></body>
     </html>
   );
 }
