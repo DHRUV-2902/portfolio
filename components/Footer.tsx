b3683b56b06caf1113cfe44204304745b8140929
Tanishka <tanishkadeep09@gmail.com>
2024-12-20 10:19:04 +0530

    project update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { FaLink, FaLinkedinIn, FaXTwitter } from "react-icons/fa6";
 import { LuGithub } from "react-icons/lu";
 import { MdOutlineEmail } from "react-icons/md";
+import { SiBuymeacoffee } from "react-icons/si";
 
 const Footer = () => {
   return (
@@ -11,7 +12,7 @@ const Footer = () => {
           <FaLink className="size-5" />
           <div className="text-xl xl:text-2xl"> Connect with me</div>
         </div>
-        <div className="flex items-center sm:gap-2 justify-center text-lg text-neutral-400 font-medium flex-col sm:flex-row">
+        <div className="flex items-start justify-center sm:gap-2 text-lg text-neutral-400 font-medium flex-col sm:flex-row">
           <div className="flex-1 w-full">
             <Link
               className="flex items-center gap-2 justify-start mb-2  hover:text-white hover:underline"
@@ -29,6 +30,15 @@ const Footer = () => {
             >
               <FaXTwitter className="size-5" />
               <div> @yestanishka</div>
+            </Link>
+
+            <Link
+              className="flex items-center gap-2 justify-start mb-2 hover:text-white hover:underline"
+              href="https://buymeacoffee.com/tanishkadeep"
+              target="_blank"
+            >
+              <SiBuymeacoffee className="size-5" />
+              <div> tanishkadeep </div>
             </Link>
           </div>
           <div className="flex-1 w-full">
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,8 +1,9 @@
 import { FaGithub } from "react-icons/fa6";
 import Image from "next/image";
-import Regal from "@/public/images/regal.jpg";
-import Bytes from "@/public/images/bytes.jpg";
-import Page from "@/public/images/page.jpg";
+import Regal from "@/public/images/regal.png";
+import Bytes from "@/public/images/bytes.png";
+import ContiqAI from "@/public/images/contiqai.png";
+import Page from "@/public/images/page.png";
 import Link from "next/link";
 
 const Projects = () => {
@@ -12,13 +13,13 @@ const Projects = () => {
         Projects
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-        <div className="bg-foreground border-neutral-600 border-2 rounded-xl p-4">
+        <div className="bg-foreground border-neutral-600 border-2 rounded-xl p-4 order-1">
           <div className="overflow-hidden rounded-lg">
             <Link href={"https://regal-estate.vercel.app/"} target="_blank">
               <Image
                 src={Regal}
                 alt="Regal Estate"
-                className="rounded-lg hover:scale-105 transition-transform duration-500"
+                className="rounded-lg hover:scale-110 transition-transform duration-500"
               />
             </Link>
           </div>
@@ -62,7 +63,7 @@ const Projects = () => {
           </div>
         </div>
 
-        <div className="bg-foreground border-neutral-600 border-2 rounded-xl p-4">
+        <div className="bg-foreground border-neutral-600 border-2 rounded-xl p-4 order-3">
           <div className="overflow-hidden rounded-lg">
             <Link
               href={"https://landing-page-rho-hazel.vercel.app/"}
@@ -71,7 +72,7 @@ const Projects = () => {
               <Image
                 src={Page}
                 alt="Page"
-                className="rounded-lg hover:scale-105 transition-transform duration-500"
+                className="rounded-lg hover:scale-110 transition-transform duration-500"
               />
             </Link>
           </div>
@@ -109,13 +110,13 @@ const Projects = () => {
           </div>
         </div>
 
-        <div className="bg-foreground border-neutral-600 border-2 rounded-xl p-4">
+        <div className="bg-foreground border-neutral-600 border-2 rounded-xl p-4 order-4">
           <div className="overflow-hidden rounded-lg">
             <Link href={"https://bytes-kohl.vercel.app/"} target="_blank">
               <Image
                 src={Bytes}
                 alt="Bytes"
-                className="rounded-lg hover:scale-105 transition-transform duration-500"
+                className="rounded-lg hover:scale-110 transition-transform duration-500"
               />
             </Link>
           </div>
@@ -158,6 +159,58 @@ const Projects = () => {
             </div>
             <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
               Zod
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-foreground border-neutral-600 border-2 rounded-xl p-4 order-2">
+          <div className="overflow-hidden rounded-lg">
+            <Link href={"https://contiq-ai.vercel.app/"} target="_blank">
+              <Image
+                src={ContiqAI}
+                alt="ContiqAI"
+                className="rounded-lg hover:scale-110 transition-transform duration-500"
+              />
+            </Link>
+          </div>
+          <div className="flex justify-between items-center gap-2 pt-4 pb-2 px-3">
+            <div className="text-2xl font-bold font-inter">ContiqAI</div>
+            <Link
+              href={"https://github.com/tanishkadeep/contiqAI"}
+              target="_blank"
+            >
+              <FaGithub className="hover:scale-110 size-6" />
+            </Link>
+          </div>
+          <div className="px-3 text-neutral-400 italic">
+            {" "}
+            AI-powered tool to quickly create high-quality content for social
+            media.
+          </div>
+          <div className="flex flex-wrap gap-2 px-3 pt-4 text-xs lg:text-sm font-medium mb-2 items-center justify-start">
+            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
+              Next.js
+            </div>
+            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
+              PostgreSQL
+            </div>
+            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
+              Prisma
+            </div>
+            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
+              Google Gemini
+            </div>
+            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
+              TypeScript
+            </div>
+            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
+              Tailwind CSS
+            </div>
+            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
+              shadcn/ui
+            </div>
+            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
+              Vercel
             </div>
           </div>
         </div>
