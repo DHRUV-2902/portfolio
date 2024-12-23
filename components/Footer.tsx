17398125ad1ec94b376aa80eb7d13490e3bbb860
Tanishka <tanishkadeep09@gmail.com>
2024-12-23 05:41:11 +0530

    refactored code

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,62 +5,80 @@ import { MdOutlineEmail } from "react-icons/md";
 import { SiBuymeacoffee } from "react-icons/si";
 import { GridPatternDashed } from "./ui/GridPatternDashed";
 
+const socials = [
+  {
+    name: "Github",
+    href: "https://github.com/tanishkadeep",
+    displayText: "tanishkadeep",
+    icon: LuGithub,
+  },
+  {
+    name: "Twitter",
+    href: "https://x.com/yestanishka",
+    displayText: "@yestanishka",
+    icon: FaXTwitter,
+  },
+  {
+    name: "Buy Me a Coffee",
+    href: "https://buymeacoffee.com/tanishkadeep",
+    displayText: "tanishkadeep",
+    icon: SiBuymeacoffee,
+  },
+  {
+    name: "Email",
+    href: "mailto:tanishkadeep09@gmail.com",
+    displayText: "tanishkadeep09@gmail.com",
+    icon: MdOutlineEmail,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/tanishkadeep/",
+    displayText: "Tanishka Deep",
+    icon: FaLinkedinIn,
+  },
+];
+
+const SocialLink = ({
+  href,
+  displayText,
+  Icon,
+  isEmail,
+}: {
+  href: string;
+  displayText: string;
+  Icon: any;
+  isEmail: boolean;
+}) => (
+  <div className="flex items-center gap-2">
+    <Link
+      href={href}
+      target="_blank"
+      className="flex items-center gap-2 hover:text-white hover:underline"
+    >
+      <Icon className="size-5" />
+      <div className={isEmail ? "break-all" : ""}>{displayText}</div>
+    </Link>
+  </div>
+);
+
 const Footer = () => {
   return (
     <GridPatternDashed>
       <div className="px-8 md:px-20 sm:px-10 py-12 max-w-6xl mx-auto">
         <div className="flex items-center gap-2 justify-start mb-2.5 font-bold">
           <FaLink className="size-5" />
-          <div className="text-xl xl:text-2xl"> Connect with me</div>
+          <div className="text-xl xl:text-2xl">Connect with me</div>
         </div>
-        <div className="flex items-start justify-center sm:gap-2 text-lg text-neutral-400 font-medium flex-col sm:flex-row">
-          <div className="flex-1 w-full">
-            <Link
-              className="flex items-center gap-2 justify-start mb-2  hover:text-white hover:underline"
-              href="https://github.com/tanishkadeep"
-              target="_blank"
-            >
-              <LuGithub className="size-5" />
-              <div> tanishkadeep</div>
-            </Link>
-
-            <Link
-              className="flex items-center gap-2 justify-start mb-2 hover:text-white hover:underline"
-              href="https://x.com/yestanishka"
-              target="_blank"
-            >
-              <FaXTwitter className="size-5" />
-              <div> @yestanishka</div>
-            </Link>
-
-            <Link
-              className="flex items-center gap-2 justify-start mb-2 hover:text-white hover:underline"
-              href="https://buymeacoffee.com/tanishkadeep"
-              target="_blank"
-            >
-              <SiBuymeacoffee className="size-5" />
-              <div> tanishkadeep </div>
-            </Link>
-          </div>
-          <div className="flex-1 w-full">
-            <Link
-              className="flex items-center gap-2 justify-start mb-2  hover:text-white hover:underline flex-wrap"
-              href="mailto:tanishkadeep09@gmail.com"
-              target="_blank"
-            >
-              <MdOutlineEmail className="size-5" />
-              <div className="break-all"> tanishkadeep09@gmail.com</div>
-            </Link>
-
-            <Link
-              className="flex items-center gap-2 justify-start mb-2  hover:text-white hover:underline"
-              href="https://www.linkedin.com/in/tanishkadeep/"
-              target="_blank"
-            >
-              <FaLinkedinIn className="size-5" />
-              <div> Tanishka Deep</div>
-            </Link>
-          </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-lg text-neutral-400 font-medium">
+          {socials.map(({ name, href, displayText, icon: Icon }, index) => (
+            <SocialLink
+              key={index}
+              href={href}
+              displayText={displayText}
+              Icon={Icon}
+              isEmail={name === "Email"}
+            />
+          ))}
         </div>
       </div>
     </GridPatternDashed>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,6 +6,72 @@ import ContiqAI from "@/public/images/contiqai.png";
 import Page from "@/public/images/page.png";
 import Link from "next/link";
 
+const projects = [
+  {
+    title: "Regal Estate",
+    description:
+      "A secure full-stack real estate app with dynamic property listings, detailed property pages, and landlord contact features, enhancing user engagement and efficiency.",
+    image: Regal,
+    liveLink: "https://regal-estate.vercel.app/",
+    githubLink: "https://github.com/tanishkadeep/Regal-Estate",
+    tags: [
+      "MERN",
+      "Zod",
+      "JWT",
+      "TypeScript",
+      "Firebase",
+      "bcryptjs",
+      "Tailwind CSS",
+    ],
+  },
+
+  {
+    title: "ContiqAI",
+    description:
+      "AI-powered tool to quickly create high-quality content for social media.",
+    image: ContiqAI,
+    liveLink: "https://contiq-ai.vercel.app/",
+    githubLink: "https://github.com/tanishkadeep/contiqAI",
+    tags: [
+      "Next.js",
+      "PostgreSQL",
+      "Prisma",
+      "Google Gemini",
+      "Clerk",
+      "TypeScript",
+      "shadcn/ui",
+    ],
+  },
+
+  {
+    title: "Modern SaaS Landing Page",
+    description:
+      "A modern SaaS landing page using Tailwind CSS, TypeScript, Next.js, and Framer Motion to create a visually appealing user interface with engaging animations.",
+    image: Page,
+    liveLink: "https://landing-page-rho-hazel.vercel.app/",
+    githubLink: "https://github.com/tanishkadeep/landing-page",
+    tags: ["Next.js", "Framer Motion", "Tailwind CSS", "TypeScript"],
+  },
+  {
+    title: "Bytes",
+    description:
+      "A full-stack blogging app inspired by Medium, with JWT authentication, TypeScript and Zod validation, and scalable deployment via Vercel and Cloudflare Workers.",
+    image: Bytes,
+    liveLink: "https://bytes-kohl.vercel.app/",
+    githubLink: "https://github.com/tanishkadeep/Bytes",
+    tags: [
+      "React.js",
+      "PostgreSQL",
+      "Prisma",
+      "Hono",
+      "TypeScript",
+      "Cloudflare Workers",
+      "Vercel",
+      "Zod",
+    ],
+  },
+];
+
 const Projects = () => {
   return (
     <div className="max-w-6xl mx-auto mt-12 xl:mt-20 mb-12 px-6 sm:px-16">
@@ -13,204 +79,46 @@ const Projects = () => {
         Projects
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-        <div className="bg-foreground border-neutral-600 border-2 rounded-xl p-4 order-1">
-          <div className="overflow-hidden rounded-lg">
-            <Link href={"https://regal-estate.vercel.app/"} target="_blank">
-              <Image
-                src={Regal}
-                alt="Regal Estate"
-                className="rounded-lg hover:scale-110 transition-transform duration-500"
-              />
-            </Link>
-          </div>
-          <div className="flex justify-between items-center gap-2 pt-4 pb-2 px-3">
-            <div className="text-2xl font-bold font-inter">Regal Estate</div>
-            <Link
-              href={"https://github.com/tanishkadeep/Regal-Estate"}
-              target="_blank"
-            >
-              <FaGithub className="hover:scale-110 size-6" />
-            </Link>
-          </div>
-          <div className="px-3 text-neutral-400 italic">
-            {" "}
-            a secure full-stack real estate app with dynamic property listings,
-            detailed property pages, and landlord contact features, enhancing
-            user engagement and efficiency.
-          </div>
-          <div className="flex flex-wrap gap-2 px-3 pt-4 text-xs lg:text-sm font-medium mb-2 items-center justify-start">
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              MERN
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="bg-foreground border-neutral-600 border-2 rounded-xl p-4"
+          >
+            <div className="overflow-hidden rounded-lg">
+              <Link href={project.liveLink} target="_blank">
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  className="rounded-lg hover:scale-110 transition-transform duration-500"
+                />
+              </Link>
             </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Zod
+            <div className="flex justify-between items-center gap-2 pt-4 pb-2 px-3">
+              <Link
+                href={project.liveLink}
+                className="text-2xl font-bold font-inter"
+              >
+                {project.title}
+              </Link>
+              <Link href={project.githubLink} target="_blank">
+                <FaGithub className="hover:scale-110 size-6" />
+              </Link>
             </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              JWT
+            <div className="px-3 text-neutral-400 italic">
+              {project.description}
             </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              TypeScript
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Firebase
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              bcryptjs
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Tailwind CSS
+            <div className="flex flex-wrap gap-2 px-3 pt-4 text-xs lg:text-sm font-medium mb-2 items-center justify-start">
+              {project.tags.map((tag, tagIndex) => (
+                <div
+                  key={tagIndex}
+                  className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full"
+                >
+                  {tag}
+                </div>
+              ))}
             </div>
           </div>
-        </div>
-
-        <div className="bg-foreground border-neutral-600 border-2 rounded-xl p-4 order-3">
-          <div className="overflow-hidden rounded-lg">
-            <Link
-              href={"https://landing-page-rho-hazel.vercel.app/"}
-              target="_blank"
-            >
-              <Image
-                src={Page}
-                alt="Page"
-                className="rounded-lg hover:scale-110 transition-transform duration-500"
-              />
-            </Link>
-          </div>
-          <div className="flex justify-between items-center gap-2 pt-4 pb-2 px-3">
-            <div className="text-2xl font-bold font-inter">
-              {" "}
-              Modern SaaS Landing Page
-            </div>
-            <Link
-              href={"https://github.com/tanishkadeep/landing-page"}
-              target="_blank"
-            >
-              <FaGithub className="hover:scale-110 size-6" />
-            </Link>
-          </div>
-          <div className="px-3 text-neutral-400 italic">
-            {" "}
-            a modern SaaS landing page using Tailwind CSS, TypeScript, Next.js,
-            and Framer Motion to create a visually appealing user interface with
-            engaging animations.
-          </div>
-          <div className="flex flex-wrap gap-2 px-3 pt-4 text-xs lg:text-sm font-medium mb-2 items-center justify-start">
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Next.js
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Framer Motion
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Tailwind CSS
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              TypeScript
-            </div>
-          </div>
-        </div>
-
-        <div className="bg-foreground border-neutral-600 border-2 rounded-xl p-4 order-4">
-          <div className="overflow-hidden rounded-lg">
-            <Link href={"https://bytes-kohl.vercel.app/"} target="_blank">
-              <Image
-                src={Bytes}
-                alt="Bytes"
-                className="rounded-lg hover:scale-110 transition-transform duration-500"
-              />
-            </Link>
-          </div>
-          <div className="flex justify-between items-center gap-2 pt-4 pb-2 px-3">
-            <div className="text-2xl font-bold font-inter">Bytes</div>
-            <Link
-              href={"https://github.com/tanishkadeep/Bytes"}
-              target="_blank"
-            >
-              <FaGithub className="hover:scale-110 size-6" />
-            </Link>
-          </div>
-          <div className="px-3 text-neutral-400 italic">
-            {" "}
-            a full-stack blogging app inspired by Medium, with JWT
-            authentication, TypeScript and Zod validation, and scalable
-            deployment via Vercel and Cloudflare Workers.
-          </div>
-          <div className="flex flex-wrap gap-2 px-3 pt-4 text-xs lg:text-sm font-medium mb-2 items-center justify-start">
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              React.js
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              PostgreSQL
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Prisma
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Hono
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              TypeScript
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Cloudflare Workers
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Vercel
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Zod
-            </div>
-          </div>
-        </div>
-
-        <div className="bg-foreground border-neutral-600 border-2 rounded-xl p-4 order-2">
-          <div className="overflow-hidden rounded-lg">
-            <Link href={"https://contiq-ai.vercel.app/"} target="_blank">
-              <Image
-                src={ContiqAI}
-                alt="ContiqAI"
-                className="rounded-lg hover:scale-110 transition-transform duration-500"
-              />
-            </Link>
-          </div>
-          <div className="flex justify-between items-center gap-2 pt-4 pb-2 px-3">
-            <div className="text-2xl font-bold font-inter">ContiqAI</div>
-            <Link
-              href={"https://github.com/tanishkadeep/contiqAI"}
-              target="_blank"
-            >
-              <FaGithub className="hover:scale-110 size-6" />
-            </Link>
-          </div>
-          <div className="px-3 text-neutral-400 italic">
-            {" "}
-            AI-powered tool to quickly create high-quality content for social
-            media.
-          </div>
-          <div className="flex flex-wrap gap-2 px-3 pt-4 text-xs lg:text-sm font-medium mb-2 items-center justify-start">
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Next.js
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              PostgreSQL
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Prisma
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Google Gemini
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              Clerk
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              TypeScript
-            </div>
-            <div className="bg-[#2e2e2e] px-3 py-0.5 w-fit rounded-full">
-              shadcn/ui
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
